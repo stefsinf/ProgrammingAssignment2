8ee017cfe6cabeedb69f4e2824c382345d7d2e08
stefsinf <samara.stefan@gmail.com>
2016-08-12 12:42:18 +0300

    courser prog assignment
    week 3 programming assignment after the example given

--- README.md
+++ README.md
@@ -44,7 +44,8 @@ really a list containing a function to
     }
 
 The following function calculates the mean of the special "vector"
-created with the above function. However, it first checks to see if the
+created with the above function. Howe
+ver, it first checks to see if the
 mean has already been calculated. If so, it `get`s the mean from the
 cache and skips the computation. Otherwise, it calculates the mean of
 the data and sets the value of the mean in the cache via the `setmean`
--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+##this function is used to cache the inverse of the matrix
+makeCacheMatrix <- function(x = matrix(1:4,nrow=2,ncol=2)) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
 
 ## Write a short comment describing this function
+##calculate the inverse if the matrix is not squared
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getinverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data,...)
+  m
 }
